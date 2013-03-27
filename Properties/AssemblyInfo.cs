50c03a97e50f4b7971d46d434c0510db0ac4a394
Kenneth Skovhede <kenneth@hexad.dk>
2013-03-27 15:10:46 +0100

    Cleaned up builder and added package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("ResxDesignerGenerator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ResxDesignerGenerator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("HexaD")]
+[assembly: AssemblyProduct("ResxDesignerGenerator")]
+[assembly: AssemblyCopyright("HexaD")]
+[assembly: AssemblyTrademark("ResxDesignerGenerator")]
+[assembly: AssemblyCulture("en-US")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
